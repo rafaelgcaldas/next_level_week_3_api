438fe640c30521f82c4154c7300ec0a15817dbde
rafaelgcaldas <rafaelzinhogomes22@live.com>
2020-10-15 22:30:55 -0300

    Init project

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import './database/connection';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (req, res) =>{
+    return res.json({ messge: "Hello world!"})
+});
+//35
+app.listen(3333);
